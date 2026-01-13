b157bcb053aa590b326087f62c79eff98ead51e4
Mrunal Awate <mrunalawate0tv@gmail.com>
2026-01-14 01:13:36 +0530

    adding some comments

--- alumni-frontend/src/navigation/app/AppTabs.js
+++ alumni-frontend/src/navigation/app/AppTabs.js
@@ -6,12 +6,12 @@ import ProfileScreen from '../../screens/ProfileScreen';
 
 const Tab = createBottomTabNavigator();                 // Create a bottom tab navigator
 
-export default function AppTabs() {
+export default function AppTabs() {             // AppTabs component  
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Members" component={MembersScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} />               // Home tab
+      <Tab.Screen name="Members" component={MembersScreen} />         // Members tab
+      <Tab.Screen name="Profile" component={ProfileScreen} />         // Profile tab
     </Tab.Navigator>
   );
 }
