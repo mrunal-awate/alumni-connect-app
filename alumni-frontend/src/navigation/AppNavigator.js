6f98c790e5b8c4526a9626ce9968a7468a59c59b
Mrunal Awate <mrunalawate0tv@gmail.com>
2026-01-15 04:17:36 +0530

    changes in file

--- alumni-frontend/src/navigation/AppNavigator.js
+++ alumni-frontend/src/navigation/AppNavigator.js
@@ -50,44 +50,98 @@
 
 
 
+// import React, { useContext } from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { AuthContext } from "../context/AuthContext";
+
+// /* ----------- Screens ----------- */
+// import LoginScreen from "../screens/auth/LoginScreen";
+// import RegisterScreen from "../screens/auth/RegisterScreen";
+
+// import HomeScreen from "../screens/HomeScreen";
+// import ProfileScreen from "../screens/profile/ProfileScreen";
+// import AlumniListScreen from "../screens/alumni/AlumniListScreen";
+// import AlumniDetailScreen from "../screens/alumni/AlumniDetailScreen";
+
+// const Stack = createNativeStackNavigator();
+
+// export default function RootNavigator() {
+//   const { user, loading } = useContext(AuthContext);
+
+//   if (loading) return null;
+
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator screenOptions={{ headerShown: false }}>
+//         {user ? (
+//           <>
+//             <Stack.Screen name="Home" component={HomeScreen} />
+//             <Stack.Screen name="Profile" component={ProfileScreen} />
+//             <Stack.Screen name="AlumniList" component={AlumniListScreen} />
+//             <Stack.Screen name="AlumniDetail" component={AlumniDetailScreen} />
+//           </>
+//         ) : (
+//           <>
+//             <Stack.Screen name="Login" component={LoginScreen} />
+//             <Stack.Screen name="Register" component={RegisterScreen} />
+//           </>
+//         )}
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+// ----------------------------- upper one is main ------ changes start from below here  2nd--------------------
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { useContext } from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthContext } from "../context/AuthContext";
 
-/* ----------- Screens ----------- */
 import LoginScreen from "../screens/auth/LoginScreen";
 import RegisterScreen from "../screens/auth/RegisterScreen";
-
-import HomeScreen from "../screens/HomeScreen";
-import ProfileScreen from "../screens/profile/ProfileScreen";
-import AlumniListScreen from "../screens/alumni/AlumniListScreen";
 import AlumniDetailScreen from "../screens/alumni/AlumniDetailScreen";
+import AppTabs from "./AppTabs";
 
 const Stack = createNativeStackNavigator();
 
-export default function RootNavigator() {
+export default function AppNavigator() {
   const { user, loading } = useContext(AuthContext);
 
   if (loading) return null;
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {user ? (
-          <>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Profile" component={ProfileScreen} />
-            <Stack.Screen name="AlumniList" component={AlumniListScreen} />
-            <Stack.Screen name="AlumniDetail" component={AlumniDetailScreen} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {user ? (
+        <>
+          <Stack.Screen name="AppTabs" component={AppTabs} />
+          <Stack.Screen name="AlumniDetail" component={AlumniDetailScreen} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+        </>
+      )}
+    </Stack.Navigator>
   );
 }
