327d4fc3e21b6fc510836de9d4b39ab75f9671fb
Mrunal Awate <mrunalawate0tv@gmail.com>
2026-01-15 04:15:51 +0530

    changes in file

--- alumni-frontend/src/navigation/app/AppTabs.js
+++ alumni-frontend/src/navigation/app/AppTabs.js
@@ -1,17 +1,79 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../../screens/HomeScreen';
-import MembersScreen from '../../screens/MembersScreen';
-import ProfileScreen from '../../screens/ProfileScreen';
+// import React from 'react';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import HomeScreen from '../../screens/HomeScreen';
+// import MembersScreen from '../../screens/MembersScreen';
+// import ProfileScreen from '../../screens/ProfileScreen';
 
-const Tab = createBottomTabNavigator();                 // Create a bottom tab navigator
+// const Tab = createBottomTabNavigator();                 // Create a bottom tab navigator
 
-export default function AppTabs() {             // AppTabs component  
+// export default function AppTabs() {             // AppTabs component  
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Home" component={HomeScreen} />               // Home tab
+//       <Tab.Screen name="Members" component={MembersScreen} />         // Members tab
+//       <Tab.Screen name="Profile" component={ProfileScreen} />         // Profile tab
+//     </Tab.Navigator>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------- upper one is main ------ changes start from below here --------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import HomeScreen from "../screens/HomeScreen";
+import AlumniListScreen from "../screens/alumni/AlumniListScreen";
+import ProfileScreen from "../screens/profile/ProfileScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function AppTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />               // Home tab
-      <Tab.Screen name="Members" component={MembersScreen} />         // Members tab
-      <Tab.Screen name="Profile" component={ProfileScreen} />         // Profile tab
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let icon;
+          if (route.name === "Home") icon = "home";
+          if (route.name === "Alumni") icon = "people";
+          if (route.name === "Profile") icon = "person";
+          return <Ionicons name={icon} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#2563eb",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Alumni" component={AlumniListScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 }
